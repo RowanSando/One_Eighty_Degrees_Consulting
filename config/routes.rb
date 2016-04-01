3129f08af299f1aca46adc330daaeeec8e3b8b1a
fununloaded <rowan.sandhu@berkeley.edu>
2016-04-01 02:41:44 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'main#index'
   devise_for :users
-  devise_scope :users do  
+  devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end
   resources :applications, only: [:index]
