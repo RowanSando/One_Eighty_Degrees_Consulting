b61a29bd2fac65a9e65f273bf781d153d439358f
fununloaded <rowan.sandhu@berkeley.edu>
2016-04-02 01:48:53 +0000

    what? test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
   # def authorize_admin
   #     redirect_to :back, :status => 401 unless current_user.admin
